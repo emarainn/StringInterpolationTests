5ba04ba7d1700b2038cede8b14cc11698433d045
emarainn <enimphius1@my.wctc.edu>
2023-10-12 16:50:11 -0500

    Final

--- StringInterpolationTemplate/Services/StringInterpolationService.cs
+++ StringInterpolationTemplate/Services/StringInterpolationService.cs
@@ -28,53 +28,86 @@ public class StringInterpolationService : IStringInterpolationService
 
     public string Number02()
     {
-        throw new NotImplementedException();
+        //2. 2019.01.22
+        var date = $"{_date.Now:yyyy.MM.dd}";
+        Console.WriteLine(date);
+        return date;
     }
 
     public string Number03()
     {
-        throw new NotImplementedException();
+        //3. Day 22 of January, 2019
+        var date = $"Day{_date.Now: dd} of {_date.Now:MMMM}, {_date.Now:yyyy}";
+        Console.WriteLine(date);
+        return date;
     }
 
     public string Number04()
     {
-        throw new NotImplementedException();
+        //4. Year: 2019, Month: 01, Day: 22
+        var date = $"Year: {_date.Now:yyyy}, Month: {_date.Now:MM}, Day: {_date.Now:dd}";
+        Console.WriteLine(date);
+        return date;
     }
 
     public string Number05()
     {
-        throw new NotImplementedException();
+        //5. Tuesday (10 spaces total, right aligned)
+        var date = $"{_date.Now,10:dddd}";
+        Console.WriteLine(date);
+        return date;
     }
 
     public string Number06()
     {
-        throw new NotImplementedException();
+        //6. 11:01 PM             Tuesday (10 spaces total for each including the day-of-week, both right-aligned)
+        var date = $"{_date.Now,10: hh:mm tt}{_date.Now,10: dddd}";
+        Console.WriteLine(date);
+        return date;
+
     }
 
     public string Number07()
     {
-        throw new NotImplementedException();
+        //7. h:11, m:01, s:27
+        //Console.WriteLine($"h:{date:hh}, m:{date:mm}, s:{date:ss}");
+        var date = $"h:{_date.Now:hh}, m:{_date.Now:mm}, s:{_date.Now:ss}";
+        Console.WriteLine(date);
+        return date;
     }
 
     public string Number08()
     {
-        throw new NotImplementedException();
+        //8. 2019.01.22.11.01.27
+        var date = $"{_date.Now:yyy}.{_date.Now:mm}.{_date.Now:dd}.{_date.Now:hh}.{_date.Now:mm}.{_date.Now:ss}";
+        Console.WriteLine(date);
+        return date;
     }
 
     public string Number09()
     {
-        throw new NotImplementedException();
+        var pi = Math.PI;
+        var ans = pi.ToString("C");
+        Console.WriteLine(ans);
+        return ans;
     }
 
     public string Number10()
     {
-        throw new NotImplementedException();
+        //10.Output as right - aligned(10 spaces), number with 3 decimal places
+        var pi = Math.PI;
+        var value = String.Format($"{pi,10:f3}");
+        Console.WriteLine(value);
+        return value;
+
     }
 
     public string Number11()
     {
-        throw new NotImplementedException();
+        //11.(2019) as a hexidecimal value
+        var year = Convert.ToUInt32($"{_date.Now:yyyy}");
+        var value = $"{year:X2}";
+        Console.WriteLine(value);
+        return value;
     }
-
-    //2.2019.01.22
 }
